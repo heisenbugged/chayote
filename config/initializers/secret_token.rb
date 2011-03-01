116a1b74c7a5f3ece8dfbb2f6907227f71f541f2
James Strong <jstrong@james-mac.gateway.2wire.net>
2011-03-01 17:07:44 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chayote::Application.config.secret_token = 'db5bd4cc03f17bbf72568b7f511b45c72b0dd8bf1e0ed76f3a530a58fe77785f924ede4fbf43d734ef8906a96e5c33fe24207c720e442934ce3fdd08eb6b3041'
