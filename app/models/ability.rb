3a0a1effb040777aedbd62cc1be4df9ecf836232
James Strong <jstrong@james-mac.gateway.2wire.net>
2011-03-08 21:43:34 -0600

    typo fix

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
     if user
       can :create, Task      
-      can :manage Task, :user_id => user.id
+      can :manage, Task, :user_id => user.id
       can :manage, TimeEntry, :user_id => user.id
       can :show, Project do |project|
         project.has_user?(user)        
