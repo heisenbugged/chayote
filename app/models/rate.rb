193541e0836a43e87e3e357aaa9c10167331d7ed
James Strong <jstrong@james-mac.gateway.2wire.net>
2011-03-15 20:13:05 -0600

    validation

--- app/models/rate.rb
+++ app/models/rate.rb
@@ -1,4 +1,6 @@
 class Rate
   include Mongoid::Document
   field :amount, :type => BigDecimal
+  validates_presence_of :amount
+  validates_numericality_of :amount, :greater_than => 0
 end
--- app/models/time_entry.rb
+++ app/models/time_entry.rb
@@ -6,7 +6,7 @@ class TimeEntry
   field :date, :type => Date
   field :hours, :type => BigDecimal
   
-  validates_numericality_of :hours
+  validates_numericality_of :hours, :greater_than => 0
 
   before_create :verify_date
   private
